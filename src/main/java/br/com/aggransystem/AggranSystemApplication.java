32409681bfe51d481e68b127ff336e4e5943c749
Lucas <mrrealcaldeira@gmail.com>
2024-10-23 21:21:46 -0300

    initial project

---
+++ src/main/java/br/com/aggransystem/AggranSystemApplication.java
@@ -0,0 +1,13 @@
+package br.com.aggransystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AggranSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AggranSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/aggransystem/AggranSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aggransystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AggranSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
